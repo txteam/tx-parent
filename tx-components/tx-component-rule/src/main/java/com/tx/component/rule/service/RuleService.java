62d009cd52dbfa7ee7c06675bae4be2ad4370d0d
240638006 <240638006@qq.com>
2013-01-18 07:58:13 +0800

    commit rule engine111

---
+++ tx-components/tx-component-rule/src/main/java/com/tx/component/rule/service/RuleService.java
@@ -0,0 +1,24 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  PengQingyang
+ * 修改时间:  2013-1-17
+ * <修改描述:>
+ */
+package com.tx.component.rule.service;
+
+
+ /**
+  * 规则引擎业务层核心类<br/>
+  * 用以生成规则容器<br/>
+  * <功能详细描述>
+  * 
+  * @author  PengQingyang
+  * @version  [版本号, 2013-1-17]
+  * @see  [相关类/方法]
+  * @since  [产品/模块版本]
+  */
+public class RuleService {
+    
+
+    
+}
