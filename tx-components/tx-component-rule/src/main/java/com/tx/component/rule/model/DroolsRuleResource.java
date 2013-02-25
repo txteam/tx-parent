a603df801b7b90564beb07d0b54874dd419681aa
240638006 <240638006@qq.com>
2013-02-26 01:24:41 +0800

    更新规则表文件

---
+++ tx-components/tx-component-rule/src/main/java/com/tx/component/rule/model/DroolsRuleResource.java
@@ -0,0 +1,23 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  PengQingyang
+ * 修改时间:  2013-2-26
+ * <修改描述:>
+ */
+package com.tx.component.rule.model;
+
+
+ /**
+  * <功能简述>
+  * <功能详细描述>
+  * 
+  * @author  PengQingyang
+  * @version  [版本号, 2013-2-26]
+  * @see  [相关类/方法]
+  * @since  [产品/模块版本]
+  */
+public class DroolsRuleResource {
+    
+    
+    
+}
