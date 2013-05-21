0b7c60fe2211c216382209ee43c054f541b8b64c
brady <brady@IT-50.bodacredit.local>
2013-05-21 13:16:27 +0800

    提交功能修改

---
+++ tx-core/src/main/java/com/tx/core/support/httpclient/annotation/ParamAlias.java
@@ -0,0 +1,49 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  brady
+ * 修改时间:  2013-5-7
+ * <修改描述:>
+ */
+package com.tx.core.support.httpclient.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 参数别名<br/>
+ *     参数别名
+ * <功能详细描述>
+ * 
+ * @author  brady
+ * @version  [版本号, 2013-5-7]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.FIELD })
+public @interface ParamAlias {
+    
+    /**
+      * 参数别名默认值<br/>
+      * <功能详细描述>
+      * @return [参数说明]
+      * 
+      * @return String [返回类型说明]
+      * @exception throws [异常类型] [异常说明]
+      * @see [类、类#方法、类#成员]
+     */
+    public String value();
+    
+    /**
+      *<功能简述>
+      *<功能详细描述>
+      * @return [参数说明]
+      * 
+      * @return Class<?> [返回类型说明]
+      * @exception throws [异常类型] [异常说明]
+      * @see [类、类#方法、类#成员]
+     */
+    public Class<?> impl() default Void.class;
+}
