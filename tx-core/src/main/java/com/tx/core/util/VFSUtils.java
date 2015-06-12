43397ecafbce0e7222299fa8deb11251c61ea506
Rain.he <Rain.he@ZSPC058.wlt.local>
2015-06-12 12:25:20 +0800

    XXX

--- tx-core/src/main/java/com/tx/core/util/VFSUtils.java
+++ tx-core/src/main/java/com/tx/core/util/VFSUtils.java
@@ -341,8 +341,8 @@ public class VFSUtils {
             out = fileObj.getContent().getOutputStream();
             IOUtils.write(data, out, encoding);
         } catch (FileSystemException e) {
-            throw new IOException("File '" + filePath + "' resolveFile fail.",
-                    e);
+            e.printStackTrace();
+            throw new IOException("File '" + filePath + "' resolveFile fail.");
         } finally {
             IOUtils.closeQuietly(out);
             if (fileObj != null) {
