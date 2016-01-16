73c7ab66350c0afe3d3f25372892632970903d08
Administrator <Administrator@USER-20150902AZ>
2016-01-16 17:58:18 +0800

    commit

---
+++ tx-components/tx-component-messagesender/src/main/java/com/tx/component/messagesender/handler/AbstractMessageSendHandler.java
@@ -0,0 +1,20 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  Administrator
+ * 修改时间:  2015年12月31日
+ * <修改描述:>
+ */
+package com.tx.component.messagesender.handler;
+
+/**
+ * 抽象消息发送处理器<br/>
+ * <功能详细描述>
+ * 
+ * @author  Administrator
+ * @version  [版本号, 2015年12月31日]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+public abstract class AbstractMessageSendHandler implements MessageSendHandler {
+    
+}
---
+++ tx-components/tx-component-messagesender/src/main/java/com/tx/component/messagesender/handler/AnnotationMessageSendHandler.java
@@ -0,0 +1,37 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  Administrator
+ * 修改时间:  2015年12月31日
+ * <修改描述:>
+ */
+package com.tx.component.messagesender.handler;
+
+import java.util.Map;
+
+import com.tx.component.messagesender.model.SendMessage;
+import com.tx.component.messagesender.model.SendResult;
+
+
+ /**
+  * <功能简述>
+  * <功能详细描述>
+  * 
+  * @author  Administrator
+  * @version  [版本号, 2015年12月31日]
+  * @see  [相关类/方法]
+  * @since  [产品/模块版本]
+  */
+public class AnnotationMessageSendHandler extends AbstractMessageSendHandler {
+    
+    /**
+     * @param message
+     * @param params
+     * @return
+     */
+    @Override
+    public SendResult send(SendMessage message, Map<String, Object> params) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
