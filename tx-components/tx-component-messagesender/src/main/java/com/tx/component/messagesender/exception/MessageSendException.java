1e78b0aed2d10b4fa16778363cc81d8e22cb15b7
Administrator <Administrator@USER-20150902AZ>
2016-01-16 17:57:14 +0800

    commit

---
+++ tx-components/tx-component-messagesender/src/main/java/com/tx/component/messagesender/exception/MessageSendException.java
@@ -0,0 +1,53 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  Rain.he
+ * 修改时间:  2015年3月30日
+ * <修改描述:>
+ */
+package com.tx.component.messagesender.exception;
+
+import com.tx.core.exceptions.SILException;
+import com.tx.core.util.MessageUtils;
+
+/**
+ * 手机应用异常
+ * 
+ * @author Rain.he
+ * @version [版本号, 2015年3月30日]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class MessageSendException extends SILException {
+    private static final long serialVersionUID = -2323505371393070811L;
+    
+    @Override
+    protected String doGetErrorCode() {
+        return "MRS_ERROR";
+    }
+    
+    @Override
+    protected String doGetErrorMessage() {
+        return "消息路由服务异常";
+    }
+    
+    public MessageSendException(Throwable cause, String message,
+            String... strings) {
+        super(MessageUtils.createMessage(message, (Object[]) strings), cause);
+    }
+    
+    public MessageSendException(String message, String... strings) {
+        super(message, (Object[]) strings);
+    }
+    
+    public MessageSendException(String message, Object[] parameters) {
+        super(message, parameters);
+    }
+    
+    public MessageSendException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public MessageSendException(String message) {
+        super(message);
+    }
+}
