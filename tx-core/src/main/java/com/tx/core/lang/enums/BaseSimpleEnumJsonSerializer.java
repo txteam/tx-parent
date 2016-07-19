6a442ef70ae4fe8d12b5ff6c6a8ec1702244039f
Administrator <Administrator@PengQingyang>
2016-07-19 23:39:22 +0800

    commit

--- tx-core/src/main/java/com/tx/core/lang/enums/BaseSimpleEnumJsonSerializer.java
+++ tx-core/src/main/java/com/tx/core/lang/enums/BaseSimpleEnumJsonSerializer.java
@@ -22,7 +22,7 @@ import org.codehaus.jackson.map.SerializerProvider;
  * @see  [相关类/方法]
  * @since  [产品/模块版本]
  */
-public class BaseSimpleEnumEnumJsonSerializer extends
+public class BaseSimpleEnumJsonSerializer extends
         JsonSerializer<BaseSimpleEnum> {
     
     /**
