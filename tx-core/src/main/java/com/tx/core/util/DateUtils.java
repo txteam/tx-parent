962425b952c1a25965cdb07d8683554b119f09d8
240638006@qq.com <Administrator@192.168.23.4>
2017-02-28 16:31:24 +0800

    工具类新版本合入

--- tx-core/src/main/java/com/tx/core/util/DateUtils.java
+++ tx-core/src/main/java/com/tx/core/util/DateUtils.java
@@ -26,7 +26,35 @@ public class DateUtils {
     /** 每天秒数 */
     private static long DAY_SECOND_COUNT = 1000 * 60 * 60 * 24;
     
-    private static long BASE_DATE_TIME = (new DateTime(1970,1,1,0,0,0).toDate()).getTime();
+    private static long BASE_DATE_TIME = (new DateTime(1970, 1, 1, 0, 0, 0).toDate()).getTime();
+    
+    /**
+      * 根据生日计算当前年龄<br/>
+      * <功能详细描述>
+      * @param birthday
+      * @return [参数说明]
+      * 
+      * @return int [返回类型说明]
+      * @exception throws [异常类型] [异常说明]
+      * @see [类、类#方法、类#成员]
+     */
+    public static int getAge(Date birthday) {
+        AssertUtils.notNull(birthday, "birthday is null");
+        
+        DateTime birthdayDateTime = new DateTime(birthday);
+        DateTime now = DateTime.now();
+        int age = 0;
+        if (now.compareTo(birthdayDateTime) < 0) {
+            age = 0;
+        } else if (now.getMonthOfYear() < birthdayDateTime.getMonthOfYear()
+                || now.getDayOfMonth() < birthdayDateTime.getDayOfMonth()) {
+            age = now.getYear() - birthdayDateTime.getYear();
+            age = age == 0 ? 0 : age - 1;
+        } else {
+            age = now.getYear() - birthdayDateTime.getYear();
+        }
+        return age;
+    }
     
     /**
       * 获取对应时间对应标准时间间隔天数
@@ -112,28 +140,35 @@ public class DateUtils {
     
     public static void main(String[] args) {
         DateTime now = DateTime.now();
-        System.out.println(calculateNumberOfDaysBetween(now.toDate(), new DateTime("2014-12-13").toDate()));
+        System.out.println(calculateNumberOfDaysBetween(now.toDate(),
+                new DateTime("2014-12-13").toDate()));
     }
     
     public static void main1(String[] args) {
         DateTime now = DateTime.now();
         System.out.println(getDays(now.toDate()));
         
-        System.out.println((new DateTime(1970,1,1,0,0,0).toDate()).getTime());
+        System.out.println((new DateTime(1970, 1, 1, 0, 0, 0).toDate()).getTime());
         
-        System.out.println(getDays(new DateTime(1970,1,1,0,0,0).toDate()));
+        System.out.println(getDays(new DateTime(1970, 1, 1, 0, 0, 0).toDate()));
         
-        System.out.println(getDays(new DateTime(now.getYear(),now.getMonthOfYear(),now.getDayOfMonth(),0,0,0).toDate()));
+        System.out.println(getDays(new DateTime(now.getYear(),
+                now.getMonthOfYear(), now.getDayOfMonth(), 0, 0, 0).toDate()));
         
-        System.out.println(getDays(new DateTime(now.getYear(),now.getMonthOfYear(),now.getDayOfMonth(),0,0,0).toDate()));
+        System.out.println(getDays(new DateTime(now.getYear(),
+                now.getMonthOfYear(), now.getDayOfMonth(), 0, 0, 0).toDate()));
         
-        System.out.println(getDays(new DateTime(now.getYear(),now.getMonthOfYear(),now.getDayOfMonth(),7,59,59).toDate()));
+        System.out.println(getDays(new DateTime(now.getYear(),
+                now.getMonthOfYear(), now.getDayOfMonth(), 7, 59, 59).toDate()));
         
         DateTime be = now.plusDays(-1);
-        System.out.println(getDays(new DateTime(be.getYear(),be.getMonthOfYear(),be.getDayOfMonth(),0,0,0).toDate()));
+        System.out.println(getDays(new DateTime(be.getYear(),
+                be.getMonthOfYear(), be.getDayOfMonth(), 0, 0, 0).toDate()));
         
-        System.out.println(getDays(new DateTime(be.getYear(),be.getMonthOfYear(),be.getDayOfMonth(),0,0,0).toDate()));
+        System.out.println(getDays(new DateTime(be.getYear(),
+                be.getMonthOfYear(), be.getDayOfMonth(), 0, 0, 0).toDate()));
         
-        System.out.println(getDays(new DateTime(be.getYear(),be.getMonthOfYear(),be.getDayOfMonth(),7,59,59).toDate()));
+        System.out.println(getDays(new DateTime(be.getYear(),
+                be.getMonthOfYear(), be.getDayOfMonth(), 7, 59, 59).toDate()));
     }
 }
