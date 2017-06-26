34f657683ffc8982267d82a98c918e38e7bf8b12
rain <rain@192.168.102.110>
2017-06-26 14:15:41 +0800

    fixbug

--- tx-core/src/main/java/com/tx/core/model/AbstractParameterizedTypeReference.java
+++ tx-core/src/main/java/com/tx/core/model/AbstractParameterizedTypeReference.java
@@ -9,24 +9,24 @@ package com.tx.core.model;
 import java.lang.reflect.Type;
 
 /**
- * 基础类能够获取该类的子类的注解类型
- * <功能详细描述>
+ * 基础类能够获取该类的子类的注解类型 <功能详细描述>
  * 
- * @author  Administrator
- * @version  [版本号, 2015年3月4日]
- * @see  [相关类/方法]
- * @since  [产品/模块版本]
+ * @author Administrator
+ * @version [版本号, 2015年3月4日]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
  */
 public abstract class AbstractParameterizedTypeReference<T> {
     
     private Type rawType;
     
-    protected void setRawType(Type rawType){
+    protected void setRawType(Type rawType) {
         this.rawType = rawType;
     }
     
-    public Type getType() {
-        return this.rawType;
+    @SuppressWarnings("unchecked")
+    public Class<T> getType() {
+        return (Class<T>) this.rawType;
     }
     
     public Type getRawType() {
