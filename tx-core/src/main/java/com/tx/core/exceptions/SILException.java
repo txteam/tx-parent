69b344a6ca1f27183fd25fda41575c3ee4737dae
zhangwei <zhangwei@leon>
2017-10-11 09:42:44 +0800

    新增构造函数，对错误码的传入

--- tx-core/src/main/java/com/tx/core/exceptions/SILException.java
+++ tx-core/src/main/java/com/tx/core/exceptions/SILException.java
@@ -88,7 +88,8 @@ public class SILException extends RuntimeException {
     public final int getErrorCode() {
         if (this.error() != null) {
             return this.error().getCode();
-        } else if (this.errorCode() != null && this.errorCode().intValue() >= 0) {
+        } else if (this.errorCode() != null
+                && this.errorCode().intValue() >= 0) {
             return this.errorCode();
         } else {
             return this.errorCode;
@@ -107,7 +108,8 @@ public class SILException extends RuntimeException {
      * @see [类、类#方法、类#成员]
      */
     public final String getErrorMessage() {
-        String errorMessage = ErrorCodeRegistry.INSTANCE.getErrorMessage(this.getErrorCode());
+        String errorMessage = ErrorCodeRegistry.INSTANCE
+                .getErrorMessage(this.getErrorCode());
         errorMessage = StringUtils.isEmpty(errorMessage) ? getMessage()
                 : errorMessage;
         return errorMessage;
@@ -121,6 +123,12 @@ public class SILException extends RuntimeException {
     /** SILException构造函数 */
     public SILException(String message) {
         super(message);
+    }
+    
+    /** SILException构造函数 */
+    public SILException(int errorCode, String message) {
+        super(message);
+        this.setErrorCode(errorCode);
     }
     
     /** SILException构造函数 */
@@ -142,8 +150,8 @@ public class SILException extends RuntimeException {
                 .append(this.getErrorMessage())
                 .append("\n");
         sb.append("\t message: ").append(super.getMessage()).append("\n");
-        sb.append("\t cause: ").append(super.getCause() == null ? ""
-                : super.getCause().toString());
+        sb.append("\t cause: ").append(
+                super.getCause() == null ? "" : super.getCause().toString());
         return sb.toString();
     }
 }
