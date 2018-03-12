42ebe34563b7b72570cdab90fd63dfea39371cd3
Administrator <Administrator@windows10.microdone.cn>
2018-03-12 23:55:06 +0800

    commit new meesage send channel

---
+++ tx-components/tx-component-communication/src/main/java/com/tx/component/communication/senddialect/sms/ronglian/RongLianDemo.java
@@ -0,0 +1,21 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  Administrator
+ * 修改时间:  2018年2月10日
+ * <修改描述:>
+ */
+package com.tx.component.communication.senddialect.sms.ronglian;
+
+
+ /**
+  * <功能简述>
+  * <功能详细描述>
+  * 
+  * @author  Administrator
+  * @version  [版本号, 2018年2月10日]
+  * @see  [相关类/方法]
+  * @since  [产品/模块版本]
+  */
+public class RongLianDemo {
+    
+}
