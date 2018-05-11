5134f5d01a25e144315027fc94a86fe20a5afb02
Administrator <Administrator@windows10.microdone.cn>
2018-05-11 11:02:20 +0800

    commit session

---
+++ tx-components/tx-component-servicelog/src/main/java/com/tx/component/servicelog/model/ServiceLogSaveTypeEnum.java
@@ -0,0 +1,25 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  Administrator
+ * 修改时间:  2018年5月11日
+ * <修改描述:>
+ */
+package com.tx.component.servicelog.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.tx.core.support.json.BaseEnum;
+import com.tx.core.support.json.BaseEnumJsonSerializer;
+
+/**
+  * <功能简述>
+  * <功能详细描述>
+  * 
+  * @author  Administrator
+  * @version  [版本号, 2018年5月11日]
+  * @see  [相关类/方法]
+  * @since  [产品/模块版本]
+  */
+@JsonSerialize(using = BaseEnumJsonSerializer.class)
+public enum ServiceLogSaveTypeEnum implements BaseEnum{
+    
+}
