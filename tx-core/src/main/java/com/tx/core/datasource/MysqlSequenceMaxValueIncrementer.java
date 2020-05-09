ca1babb88e68e31e9cf97a929f20dc6485bd0ee5
PengQingyang <PengQingyang@DESKTOP-4CCI9AD>
2020-05-09 17:18:29 +0800

    commit mysql的sequence实现

---
+++ tx-core/src/main/java/com/tx/core/datasource/MysqlSequenceMaxValueIncrementer.java
@@ -0,0 +1,42 @@
+/*
+ * 描          述:  <描述>
+ * 修  改   人:  PengQingyang
+ * 修改时间:  2020年5月9日
+ * <修改描述:>
+ */
+package com.tx.core.datasource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.incrementer.AbstractSequenceMaxValueIncrementer;
+
+/**
+ * MysqlSequence的实现<br/>
+ * <功能详细描述>
+ * 
+ * @author  PengQingyang
+ * @version  [版本号, 2020年5月9日]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+public class MysqlSequenceMaxValueIncrementer
+        extends AbstractSequenceMaxValueIncrementer{
+    
+    private JdbcTemplate jdbcTemplate;
+    
+    /**
+     * 
+     */
+    @Override
+    public void afterPropertiesSet() {
+        super.afterPropertiesSet();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    protected String getSequenceQuery() {
+        return "select nextval(" + getIncrementerName() + ")";
+    }
+    
+}
